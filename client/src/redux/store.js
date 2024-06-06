0e48c5d7f737b7fcfaf85a2153192aed04df42d8
wallbreaker01 <srshowmik2001@gmail.com>
2024-06-06 21:23:12 +0600

    4th commit

---
+++ client/src/redux/store.js
@@ -0,0 +1,24 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+;
+import userReducer from './user/userSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { version } from 'mongoose';
+const rootReducer = combineReducers({user: userReducer});
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: {user: userReducer},
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,32 @@
+import { createSlice, current} from '@reduxjs/toolkit';
+import e from 'express';
+import { sign } from 'jsonwebtoken';
+
+const initialState={
+    currentUser:null,
+    error:null,
+    loading:false,
+};
+const userSlice = createSlice({
+    name:'user',
+    initialState,
+    reducers:{
+        signInStart:(state)=>{
+            state.loading=true;
+        },
+        signInSuccess:(state,action)=>{
+            
+            state.currentUser=action.payload;
+            state.loading=false;
+            state.error=null;
+        },
+        signInFailure:(state,action)=>{
+            state.error=action.payload;
+            state.loading=false;
+        },
+    },
+});
+
+export const {signInStart,signInSuccess,signInFailure}=userSlice.actions;
+
+export default userSlice.reducer;
