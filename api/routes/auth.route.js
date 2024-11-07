6ccb712b72a3388be65aaca849460beaaccbea26
Md Mustakin Ebne Mazid <mustakinshvn@gmail.com>
2024-11-08 02:17:29 +0600

    Create signIn API route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
-import { signUp } from '../controllers/auth.controller.js';
+import { signUp, signIn } from '../controllers/auth.controller.js';
 import express from 'express';
 
 const router = express.Router()
 
-router.post('/sign-up', signUp)
+router.post('/sign-up', signUp);
+router.post('/sign-in', signIn);
 
 export default router;
