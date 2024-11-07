63da89e5479a4dd7fb9f7c5749c3622a610df47f
Md Mustakin Ebne Mazid <mustakinshvn@gmail.com>
2024-11-07 17:58:42 +0600

    Connected to database

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(() => {
+  console.log('Connected to Database');
+}) 
+.catch((error) => {
+  console.log(error)
+});
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
